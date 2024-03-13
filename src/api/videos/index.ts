424dbc4e57121ca73a9fcf7f7bf711581f2a3ef2
Ffinnis <potapovroman03@gmail.com>
2024-03-13 22:00:16 +0700

    (feat) add env

--- src/api/videos/index.ts
+++ src/api/videos/index.ts
@@ -2,7 +2,9 @@ import { VideoData } from './types';
 
 class VideoApi {
   async getVideos(title?: string): Promise<VideoData[]> {
-    const url = new URL(`http://localhost:3333/api/v1/video?${title ? `title=${title}` : ''}`);
+    const url = new URL(
+      `${import.meta.env.VITE_BACKEND_API_URL}/api/v1/video?${title ? `title=${title}` : ''}`,
+    );
 
     const req = await fetch(url, {
       method: 'GET',
